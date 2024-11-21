436761eff605964844c108b2d71bd5fe3eb0080c
DaveCuc <l21360939@tehuacan.tecnm.mx>
2024-11-20 19:58:15 -0600

    hoja de estilos y scripts
    modificaciones

---
+++ Page/scripts.js
@@ -0,0 +1,17 @@
+
+    function toggleDropdown() {
+        const dropdown = document.getElementById('dropdown');
+        if (dropdown.style.display === 'none') {
+            dropdown.style.display = 'block';
+        } else {
+            dropdown.style.display = 'none';
+        }
+    }
+
+    // Opcional: cerrar el menú si se hace clic fuera
+    window.onclick = function(event) {
+        const dropdown = document.getElementById('dropdown');
+        if (!event.target.matches('.icon')) {
+            dropdown.style.display = 'none';
+        }
+    }
