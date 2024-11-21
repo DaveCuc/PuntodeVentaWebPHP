c76064835f89f6d3f25c3ecb4333feb26450b7e7
DaveCuc <l21360939@tehuacan.tecnm.mx>
2024-11-20 22:34:20 -0600

    yeei

--- Page/scripts.js
+++ Page/scripts.js
@@ -15,3 +15,20 @@
             dropdown.style.display = 'none';
         }
     }
+
+
+        function toggleDropdown() {
+            const dropdown = document.getElementById('dropdown');
+            if (dropdown.style.display === 'none' || dropdown.style.display === '') {
+                dropdown.style.display = 'block';
+            } else {
+                dropdown.style.display = 'none';
+            }
+        }
+
+        window.onclick = function(event) {
+            const dropdown = document.getElementById('dropdown');
+            if (!event.target.matches('.icon')) {
+                dropdown.style.display = 'none';
+            }
+        }
